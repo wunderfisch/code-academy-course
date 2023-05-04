6dfaed64ab329bb03c67481282b82e69450c2e4e
wunderfisch <wunderfisch@posteo.de>
2023-05-04 17:29:25 +0200

    further React

--- 3_sprint/react-firstweek/rick_morty/rick-morty-project/src/views/Characters.js
+++ 3_sprint/react-firstweek/rick_morty/rick-morty-project/src/views/Characters.js
@@ -7,7 +7,7 @@ function Characters() {
   // fetch data from API
   const getCharacters = () => {
     // url can be in a own variable
-    let url = "https://rickandmortyapi.com/api/character";
+    let url = "https://rickandmortyapi.com/api/character/?page=2";
 
     // regular fetch
     fetch(url)
@@ -18,6 +18,7 @@ function Characters() {
         console.log("data :>> ", data);
         //
         setCharacters(data.results);
+        /* createCards(data.results); */
       })
       .catch((error) => {
         console.log("error :>> ", error);
@@ -28,9 +29,50 @@ function Characters() {
     getCharacters();
   }, []);
 
+  /* Search bar */
+  // state variable for temporary storing input
+  // initialise as empty string because it will be a string
+  const [inputText, setInputText] = useState("");
+
+  // handler
+  let onInputChangeHandler = (event) => {
+    setInputText(event.target.value);
+  };
+
+  /*  function createCards(data) {
+    let [cards, setCards] = useState();
+  } */
+
   return (
     <div>
       <h2>Characters</h2>
+      {/* search bar */}
+      <section>
+        <input
+          type="text"
+          value={inputText}
+          onChange={onInputChangeHandler}
+        ></input>
+      </section>
+      {/* section to hold all cards */}
+      <section>
+        {/* singel card */}
+        <div>
+          {/* image */}
+          {characters ? (
+            characters.map((firstparameter, index) => {
+              return (
+                <div key={firstparameter.id}>
+                  <img src={firstparameter.image}></img>
+                  <p>{firstparameter.name}</p>
+                </div>
+              );
+            })
+          ) : (
+            <p>loading...</p>
+          )}
+        </div>
+      </section>
       {/*   {characters &&
         characters.map((firstparameter, index) => {
           // fetch is by default asynchronous, when the compiler arrives here the setCharacters(data.results) in second .then has not run yet
@@ -44,15 +86,28 @@ function Characters() {
           );
         })} */}
       {/*  instead of conditinal rendering we can also use a ternary operator. if characters is true go on... */}
-      {characters ? (
-        characters.map((firstparameter) => {
-          return <p>{firstparameter.name}</p>;
+      {/* {characters ? (
+        characters.map((firstparameter, index) => {
+          return <div><p key={index}>{firstparameter.name}</p>
+          <p></p>
+          </div>
+          
+          
         })
       ) : (
         <p>loading...</p>
-      )}
+      )} */}
     </div>
   );
 }
 
 export default Characters;
+
+//Next steps:
+//1. Create a card component, and build the card structure there. That compoennt should receive the character information as props
+//2. display your cards in a grid
+//3. create an show More info button...that is gonna open a modal
+//4. build your modal either by adding a div to the card , that is inittialy hidden, or by building a modal component, and pass some infor as props to indicate if it should be hidden or not.
+//5. Filtering : create an input, add a change event, and buil a function that onChange, it filters the original array according to the letters typed (using the character's name).
+//6. look for a way of mapping over the array of filtered data  (maybe think about doing it since the beggining )
+//7. Pagination: think about the concept : we fecht a new url, when we click in a button.(remember how to use the dependecy array in the useEffect...)
--- 3_sprint/react-firstweek/test-react/src/Phoenixes.js
+++ 3_sprint/react-firstweek/test-react/src/Phoenixes.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Student from "./Student";
 
 function Phoenixes() {
-  const phoenixes = [
+  const initialList = [
     "Alireza",
     "Sonja",
     "Melissa",
@@ -12,6 +12,19 @@ function Phoenixes() {
     "Phouc",
     "Carl",
   ];
+  const [group, setGroup] = useState(initialList);
+
+  // state variable for temporary storing input before sending it to array
+  // initialise as empty string because it will be a string
+  const [inputText, setInputText] = useState("");
+
+  // initial value of greet = "hey"
+  const [greet, setGreet] = useState("hey");
+  // function should run as soon as the component loads (= initial value won't be seen)
+  const updateGreet = () => {
+    setGreet("Guten Tag");
+  };
+
   // a second element to be send somewhere
   const projectName = "React";
 
@@ -22,10 +35,43 @@ function Phoenixes() {
     console.log("color :>> ", color);
   };
 
+  // handler
+  // a parameter here "onInputChangeHandler = ()" represents the event object
+  let onInputChangeHandler = (event) => {
+    console.log(event.target.value);
+    // display input with what is typed
+    setInputText(event.target.value);
+    // check that variable inputText is storing what is typed:
+    console.log("inputText :>> ", inputText);
+    // but it is always one late. It always shows the previews state before it gets updated
+    // to see the actual state the clg needs to be in the JSX part
+  };
+
+  // adding input to the array
+  // .push does not notice react that there is a difference in the variable so it is not rendering again
+  let addStudent = () => {
+    setGroup([...group, inputText]);
+  };
+
+  useEffect(() => {
+    console.log("useEffect updateGreet run first");
+    updateGreet();
+  }, [group]); // only runs when group changes
+
   return (
+    /* This is the virtual DOM = a virtual html being put in folder "public", if something gets updated, it needs to be rendered to show the new state*/
     <div>
+      <h3>{greet}</h3>
+      <div>
+        {/* add event to the input, assigne value of input to function above */}
+        <input type="text" value={inputText} onChange={onInputChangeHandler} />
+        {/* onclick call function */}
+        <button onClick={addStudent}>Add student</button>
+      </div>
+      {/* this cgl shows the actual updated state of the variable */}
+      {console.log("inputText in JSX :>> ", inputText)}
       <h3>All the Phoenixes are:</h3>
-      {phoenixes.map((phoenix, index) => {
+      {group.map((phoenix, index) => {
         return (
           /* first option */
           /*   <ul>
